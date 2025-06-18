b23d0e09684511e84c7a63cc211ed6b322847355
Tedoari <43781455+Tedoari@users.noreply.github.com>
2025-06-18 16:10:36 +0200

    NextAuth lib changes
    NextAuth lib changes

---
+++ lib/authOptions.ts
@@ -0,0 +1,66 @@
+// lib/authOptions.ts
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma"; // Your Prisma client
+import { user } from "@prisma/client";
+import { NextAuthOptions } from "next-auth";
+import { JWT } from "next-auth/jwt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user: user | null = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (user && user.password === credentials.password) {
+          return {
+            id: user.id.toString(),
+            email: user.email,
+            name: user.name,
+            role: user.role,
+            image: user.image,
+          };
+        }
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user }: { token: JWT; user?: any }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.name = user.name;
+        token.role = user.role;
+        token.image = user.image;
+      }
+      return token;
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async session({ session, token }: { session: any; token: JWT }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.name = token.name;
+        session.user.role = token.role;
+        session.user.image = token.image;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
